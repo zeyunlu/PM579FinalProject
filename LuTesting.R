397f1054d93b56ca255d9ef2441cb86744517de4
Zeyun Lu <zeyunlu@usc.edu>
2020-07-27 17:12:44 -0700

    Add fianl

---
+++ LuTesting.R
@@ -0,0 +1,23 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+a <- read_csv("data/whole.csv") %>%
+  select(-`Gene Accession Number`) %>%
+  #rownames_to_column() %>%
+  pivot_longer(-`Gene Description`,"patient", "value") %>%
+  pivot_wider(patient, `Gene Description`) %>%
+  drop_na()
+
+b <- read_csv("data/datasets_1868_3249_actual.csv") %>%
+  mutate(patient = as.character(patient)) %>%
+  left_join(a, by = "patient")
+
+b <- as.data.frame(b)
+colnames(b)
+df <- apply(b, 2, as.character)
+df <- as.data.frame(df)
+
+write_csv(df, path = "data/final.csv", quote_escape = FALSE)
+
